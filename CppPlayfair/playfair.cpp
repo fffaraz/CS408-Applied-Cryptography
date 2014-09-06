da5495945dcea81bad70dba00ce138bbc6623d73
Faraz Fallahi <fffaraz@gmail.com>
2014-09-06 12:44:17 -0500

    Created the project

---
+++ CppPlayfair/playfair.cpp
@@ -0,0 +1,5 @@
+#include "playfair.h"
+
+Playfair::Playfair()
+{
+}
---
+++ CppPlayfair/playfair.h
@@ -0,0 +1,10 @@
+#ifndef PLAYFAIR_H
+#define PLAYFAIR_H
+
+class Playfair
+{
+public:
+    Playfair();
+};
+
+#endif // PLAYFAIR_H
